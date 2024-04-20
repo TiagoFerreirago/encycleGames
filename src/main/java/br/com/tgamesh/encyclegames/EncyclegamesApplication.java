fc54e8146808867e7add26e23a9dfa5b695cb3d6
Tiago Ferreira <thdejesusferreira@gmail.comgit config --listgit config --listgit config --list git config --list--list>
2024-04-20 12:40:32 -0300

    first project

---
+++ src/main/java/br/com/tgamesh/encyclegames/EncyclegamesApplication.java
@@ -0,0 +1,13 @@
+package br.com.tgamesh.encyclegames;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EncyclegamesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EncyclegamesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/tgamesh/encyclegames/EncyclegamesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.tgamesh.encyclegames;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EncyclegamesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
